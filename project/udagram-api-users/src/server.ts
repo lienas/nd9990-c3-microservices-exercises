e7ced1232d417c31cf5dab2afaad9893841ce0d3
thomas <t.lucas@lienas.de>
2021-01-30 12:58:33 +0100

    fix cors

--- project/udagram-api-users/src/server.ts
+++ project/udagram-api-users/src/server.ts
@@ -10,35 +10,34 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
+    await sequelize.addModels(V0_USER_MODELS);
+    await sequelize.sync();
 
-  const app = express();
-  const port = process.env.PORT || 8080;
+    const app = express();
+    const port = process.env.PORT || 8080;
 
-  app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
-  }));
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+        next();
+    });
 
-  app.use('/api/v0/', IndexRouter);
+    app.use('/api/v0/', IndexRouter);
 
-  // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+    // Root URI call
+    app.get('/', async (req, res) => {
+        res.send('/api/v0/');
+    });
 
 
-  // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running ${config.url}`);
+        console.log(`press CTRL+C to stop server`);
+    });
 })();
