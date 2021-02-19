de9db44cea72ad6f1e5e5ae96c91e5c011214c72
thomas <t.lucas@lienas.de>
2021-02-19 12:15:52 +0100

    change config for kubernetes

--- project/udagram-api-feeds/src/aws.ts
+++ project/udagram-api-feeds/src/aws.ts
@@ -5,6 +5,7 @@ import {config} from './config/config';
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
+console.log("I use the following credentials: " + credentials.accessKeyId)
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -15,7 +16,7 @@ export const s3 = new AWS.S3({
 // Generates an AWS signed URL for retrieving objects
 export function getGetSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
-
+  console.log("env-vars for signed url for get = bucket:" +  config.aws_media_bucket + " und key: " + key );
   return s3.getSignedUrl('getObject', {
     Bucket: config.aws_media_bucket,
     Key: key,
@@ -26,6 +27,7 @@ export function getGetSignedUrl( key: string ): string {
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
+  console.log("env-vars for signed url for put = bucket:" +  config.aws_media_bucket + " und key: " + key );
 
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
